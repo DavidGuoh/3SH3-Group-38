5488d09cc3c5750b9768804ca6d4ce3a0f6ae9b4
DavidGuoh <113489218+DavidGuoh@users.noreply.github.com>
2023-02-18 18:13:34 -0500

    Add files via upload

--- Assignment 1/shell.c
+++ Assignment 1/shell.c
@@ -1,8 +1,3 @@
-//#include <linux/init.h>
-//#include <linux/module.h>
-//#include <linux/kernel.h>
-//#include <linux/proc_fs.h>
-//#include <asm/uaccess.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,62 +6,80 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define MAX_LINE 80
-
+#define MAX_LINE 80 //define the max length of input command
+void print_history(char history[][MAX_LINE/2+1],int history_count){
+    for(int j =0;j<history_count;j++){
+        printf("%d %s\n",j+1,history[j]);
+    }
+    return;
+}
 int main(){
     char *args[MAX_LINE/2+1];
-    char history[5][MAX_LINE/2+1];
-    int history_count = 0;
-    int should_run = 1;
-    while (should_run){
-        char cmd[20];
+    char history[5][MAX_LINE/2+1]; //Create array to store last 5 history
+    int history_count = 0; //count the number of history in list
+    int should_run = 1; //flag for continue running the program
+    while (should_run==1){ 
+        char input[20]; //store input command
         printf("%s","osh>");
-        fgets(cmd, 20, stdin);
-        fflush(stdout);
-        //printf("%s",cmd);
-        int concurrent;
+        fgets(input, 20, stdin); //read input and store
+        fflush(stdout); //flushes the output buffer of a stream
+        //printf("%s",input);
+        int concurrent; //flag for running concurrently or not
         //fflush(stdout);
         //printf("%s",checksymbol);
-        if (cmd[strlen(cmd)-2] == '&'){
+        if (input[strlen(input)-2] == '&'){ //detect symbol
             concurrent = 1;
-            cmd[strlen(cmd)-2] ='\0';
+            input[strlen(input)-2] ='\0'; //add end symbol at the end of input
         }
         else{
             concurrent = 0;
-            cmd[strlen(cmd)-1] = '\0';
+            input[strlen(input)-1] = '\0';
         }
-        
+
+        if (strcmp(input, "exit") == 0){ //exit the program if input is exit
+            should_run = 0;
+            exit(0);
+            break;
+        }
         //fflush(stdout);
         //free(checksymbol);
-        //cmd[strlen(cmd)-1] = '\0';
-        if(strcmp(cmd,"!!")==0 && history_count==0){
+        //input[strlen(input)-1] = '\0';
+        if(strcmp(input,"!!")==0 && history_count==0){ //error handling !! without history
             printf("%s\n","No commands in history.");
+            fflush(stdout);
         }
-        else if(strcmp(cmd,"!!")==0 && history_count>0){
-            strcpy(cmd,history[0]);
-        }
-        if (strcmp(cmd, "exit") == 0){
-            should_run = 0;
-        }
-        if(strcmp(cmd,"history")==0 && history_count==0){
-            printf("%s\n","No commands in history.");
-        }
-        else if(strcmp(cmd,"history")==0 && history_count!=0){
-            for(int j =0;j<history_count;j++){
-                printf("%d %s\n",history_count-j,history[j]);
+        else if(strcmp(input,"!!")==0 && history_count>0){ 
+            if (strcmp(history[0],"!!")==0){ //error handling !! call !!
+                printf("%s\n","error: !! can't call !!");
+                fflush(stdout);
+                continue;
+            }
+            else{
+                strcpy(input,history[0]); //execute the last input
             }
         }
-        else{}
-        
-        for(int i = 4;i>0;i--){
+
+        if(strcmp(input,"history")==0 && history_count==0){ //Error handling no history in history
+            printf("%s\n","No commands in history.");
+            fflush(stdout);
+        }
+        else if(strcmp(input,"history")==0 && history_count!=0){ //print history
+            print_history(history,history_count);
+            /*for(int j =0;j<history_count;j++){ //function
+                printf("%d %s\n",j+1,history[j]);
+            }*/
+        }
+        for(int i = 4;i>0;i--){  //store newest command in history
                 strcpy(history[i],history[i-1]);
             }
-            strcpy(history[0],cmd);
+            strcpy(history[0],input);
             if (history_count<5){
                 history_count++;
             }
-            else{}
-        args[0] = strtok(cmd," ");
+        if(strcmp(input,"!!")==0){ //error handling no !! should be executed by execvp
+            continue;
+        }
+        args[0] = strtok(input," "); //clean input for execvp
         int i = 1;
         while(1){
             char* tok = strtok(NULL," ");
@@ -76,21 +89,31 @@ int main(){
             args[i++] = tok;
             }
         args[i] = NULL;
-        
-        pid_t pid;
+        pid_t pid; 
         //int status;
-        pid = fork();
+        pid = fork(); // create child process
+        int status; // specify status
         //char* command = "ls"; 
         //char* argument_list[] = {"ls","-l", NULL};
-        if(pid == 0){
-            execvp(args[0],args);
+        if(pid == 0){ //Childs process run
+            execvp(args[0],args); //run the command   
+            if(strcmp(input,"history")==0 || strcmp(input, "exit") == 0 || strcmp(input,"!!")==0){}
+            else{ //error handling if command doesn't work
+                printf("%s","Not A Functional Command\n");
+                fflush(stdout);
+            }
         }
-        else if(pid < 0){
+        else if(pid < 0){ //error if child process fail to create
             printf("%s","Fork Failed");
+            fflush(stdout);
+            should_run =0;
         }
         else{
-            if (concurrent == 0){
+            if (concurrent == 0){ //If parent run after child process finish
                 wait(NULL);
+            }
+            else{ //If parent run at the same time as child process run
+                waitpid(pid,&status,WUNTRACED);//WNOHANG);WUNTRACED
             }
         }
     }
